f9cbeaa11024e72aab998aa9e9156f4ef3cc6925
leavez <gaojiji@gmail.com>
2018-06-03 18:11:22 +0800

    add UT for custom enum conversion

--- Tests/MappableTests/EnumTest.swift
+++ Tests/MappableTests/EnumTest.swift
@@ -19,6 +19,35 @@ struct EnumArrayModel: Mappable {
     }
 }
 
+
+enum EnumWithValueModel: Mappable, Equatable {
+    case a(Int)
+    case b(String)
+    
+    init(map: Mapper) throws {
+        let value = try map.getValue("type", as: String.self)
+        switch value {
+        case "a":
+            self = .a(try map.from("value"))
+        case "b":
+            self = .b(try map.from("value"))
+        default:
+            throw ErrorType.JSONStructureNotMatchDesired(value, "string a/b")
+        }
+    }
+    
+    static func == (lhs: EnumWithValueModel, rhs: EnumWithValueModel) -> Bool {
+        switch (lhs, rhs) {
+        case (.a(let a1), .a(let a2)):
+            return a1 == a2
+        case (.b(let a1), .b(let a2)):
+            return a1 == a2
+        default:
+            return false
+        }
+    }
+}
+
 class EnumTests: XCTestCase {
 
     func test() {
@@ -36,5 +65,35 @@ class EnumTests: XCTestCase {
             print(e)
             XCTAssertTrue(false)
         }
+    }
+    
+    func testCustomConversion() {
+        
+        let json = """
+        [
+            {"type": "a", "value": 123},
+            {"type": "a", "value": 0},
+            {"type": "b", "value": "hello"},
+            {"type": "b", "value": "world"},
+        ]
+        """
+        do {
+            let a = try [EnumWithValueModel](JSONString: json)
+            XCTAssertEqual(a[0], EnumWithValueModel.a(123))
+            XCTAssertEqual(a[1], EnumWithValueModel.a(0))
+            XCTAssertEqual(a[2], EnumWithValueModel.b("hello"))
+            XCTAssertEqual(a[3], EnumWithValueModel.b("world"))
+
+        } catch let e {
+            XCTFail(e.localizedDescription)
+        }
+        
+        let json2 = """
+        [
+            {"type": 123, "value": 123},
+            {"type": {}, "value": 0},
+        ]
+        """
+        XCTAssertThrowsError(try [EnumWithValueModel](JSONString: json2))
     }
 }
