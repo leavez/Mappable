5273f5f4468c8555f8c5bfa993c97050f52226a8
leavez <gaojiji@gmail.com>
2018-06-03 13:24:08 +0800

    add UT for basic types

--- Tests/MappableTests/BasicTypeTest.swift
+++ Tests/MappableTests/BasicTypeTest.swift
@@ -7,35 +7,39 @@
 
 import XCTest
 @testable import Mappable
+#if canImport(CoreGraphics)
+import CoreGraphics
+#endif
 
-class IntTypesModel: Mappable {
-
-    let int: Int
-    let int8: Int8
-    let int16: Int16
-    let int32: Int32
-    let int64: Int64
-    let uInt: UInt
-    let uInt8: UInt8
-    let uInt16: UInt16
-    let uInt32: UInt32
-    let uInt64: UInt64
-
-    required init(map: Mapper) throws {
-        int = try map.from("int")
-        int8 = try map.from("short")
-        int16 = try map.from("int")
-        int32 = try map.from("int")
-        int64 = try map.from("long")
-        uInt = try map.from("uint")
-        uInt8 = try map.from("short")
-        uInt16 = try map.from("uint")
-        uInt32 = try map.from("uint")
-        uInt64 = try map.from("long")
-    }
-}
 
 class BasicTypesTest: XCTestCase {
+
+    class IntTypesModel: Mappable {
+        
+        let int: Int
+        let int8: Int8
+        let int16: Int16
+        let int32: Int32
+        let int64: Int64
+        let uInt: UInt
+        let uInt8: UInt8
+        let uInt16: UInt16
+        let uInt32: UInt32
+        let uInt64: UInt64
+        
+        required init(map: Mapper) throws {
+            int = try map.from("int")
+            int8 = try map.from("short")
+            int16 = try map.from("int")
+            int32 = try map.from("int")
+            int64 = try map.from("long")
+            uInt = try map.from("uint")
+            uInt8 = try map.from("short")
+            uInt16 = try map.from("uint")
+            uInt32 = try map.from("uint")
+            uInt64 = try map.from("long")
+        }
+    }
 
     func testInt() {
         let json = """
@@ -87,7 +91,147 @@ class BasicTypesTest: XCTestCase {
             print(e)
             XCTAssertTrue(false)
         }
+        
+    }
+    
+
+    struct IntTypeModel2: Mappable {
+        let int: Int
+        
+        init(map: Mapper) throws {
+            int = try map.from("int")
+        }
+    }
+    
+    func testInt2() {
+        [Int.min, -999999, -1, 0, 1, 100, 99999999, Int.max].forEach { value in
+            XCTAssertEqual(try? IntTypeModel2(JSONObject: ["int": value]).int, value)
+            XCTAssertEqual(try? IntTypeModel2(JSONObject: ["int": String(value)]).int, value)
+        }
+        XCTAssertEqual(try? IntTypeModel2(JSONObject: ["int": 0x123]).int, 0x123)
+        XCTAssertThrowsError(try IntTypeModel2(JSONObject: ["int": "1.0"]))
+    }
+    
+    
+    
+    
+    class FloatTypesModel: Mappable {
+        
+        let double: Double
+        let float: Float
+        #if canImport(CoreGraphics)
+        let cgFloat: CGFloat
+        #endif
+        
+        required init(map: Mapper) throws {
+            double = try map.from("double")
+            float = try map.from("double")
+            #if canImport(CoreGraphics)
+            cgFloat = try map.from("double")
+            #endif
+        }
     }
 
+    func testFloat() {
+        do{
+            let a = try FloatTypesModel(JSONObject: ["double": "-12345.123"])
+            XCTAssertEqual(a.double, -12345.123)
+            XCTAssertEqual(a.float, -12345.123)
+            #if canImport(CoreGraphics)
+            XCTAssertEqual(a.cgFloat, -12345.123)
+            #endif
+        } catch let e {
+            print(e)
+            XCTAssertTrue(false)
+        }
+        
+        XCTAssertEqual(try? FloatTypesModel(JSONObject: ["double": 1]).double, 1)
+        XCTAssertEqual(try? FloatTypesModel(JSONObject: ["double": 1.0]).double, 1)
+        XCTAssertEqual(try? FloatTypesModel(JSONObject: ["double": 1.01]).double, 1.01)
+        XCTAssertEqual(try? FloatTypesModel(JSONObject: ["double": "1.01"]).double, 1.01)
+        XCTAssertEqual(try? FloatTypesModel(JSONObject: ["double": "-1.01"]).double, -1.01)
+        XCTAssertEqual(try? FloatTypesModel(JSONObject: ["double": "0"]).double, 0)
+        XCTAssertThrowsError(try FloatTypesModel(JSONObject: ["double": "aaa"]))
+    }
+    
+    
+    
+    
+    struct BoolModel: Mappable {
+        let bool: Bool
+        
+        init(map: Mapper) throws {
+            bool = try map.from("bool")
+        }
+    }
+    
+    func testBool() {
+        XCTAssertEqual(try? BoolModel(JSONObject: ["bool": 0]).bool, false)
+        XCTAssertEqual(try? BoolModel(JSONObject: ["bool": -1]).bool, true)
+        XCTAssertEqual(try? BoolModel(JSONObject: ["bool": 1]).bool, true)
+        XCTAssertEqual(try? BoolModel(JSONObject: ["bool": 9999]).bool, true)
+        XCTAssertEqual(try? BoolModel(JSONObject: ["bool": "YES"]).bool, true)
+        XCTAssertEqual(try? BoolModel(JSONObject: ["bool": "True"]).bool, true)
+        XCTAssertEqual(try? BoolModel(JSONObject: ["bool": "true"]).bool, true)
+        XCTAssertEqual(try? BoolModel(JSONObject: ["bool": "TRUE"]).bool, true)
+        XCTAssertEqual(try? BoolModel(JSONObject: ["bool": "1"]).bool, true)
+        XCTAssertEqual(try? BoolModel(JSONObject: ["bool": "NO"]).bool, false)
+        XCTAssertEqual(try? BoolModel(JSONObject: ["bool": "False"]).bool, false)
+        XCTAssertEqual(try? BoolModel(JSONObject: ["bool": "FALSE"]).bool, false)
+        XCTAssertEqual(try? BoolModel(JSONObject: ["bool": "false"]).bool, false)
+        XCTAssertEqual(try? BoolModel(JSONObject: ["bool": "0"]).bool, false)
+
+        XCTAssertThrowsError(try BoolModel(JSONObject: ["bool": "aaa"]).bool)
+        XCTAssertThrowsError(try BoolModel(JSONObject: ["bool": 12.34]).bool)
+        XCTAssertThrowsError(try BoolModel(JSONObject: ["bool": "2"]).bool)
+    }
+    
+    
+    
+    struct StringModel: Mappable {
+        let value: String
+        
+        init(map: Mapper) throws {
+            value = try map.from("value")
+        }
+    }
+    
+    func testString() {
+        XCTAssertEqual(try? StringModel(JSONObject: ["value": "ssss"]).value, "ssss")
+        XCTAssertEqual(try? StringModel(JSONObject: ["value": 0]).value, "0")
+        XCTAssertEqual(try? StringModel(JSONObject: ["value": 123]).value, "123")
+        XCTAssertEqual(try? StringModel(JSONObject: ["value": -1]).value, "-1")
+        XCTAssertEqual(try? StringModel(JSONObject: ["value": 123.0]).value, "123")
+        XCTAssertEqual(try? StringModel(JSONObject: ["value": 123.456]).value, "123.456")
+        XCTAssertEqual(try? StringModel(JSONObject: ["value": -123.456]).value, "-123.456")
+        
+        XCTAssertThrowsError(try StringModel(JSONObject: ["value": []]).value)
+        XCTAssertThrowsError(try StringModel(JSONObject: ["value": [:]]).value)
+    }
+
+    struct UrlModel: Mappable {
+        let value: URL
+        init(map: Mapper) throws {
+            value = try map.from("value")
+        }
+    }
+    
+    func testURL() {
+        XCTAssertEqual(try? UrlModel(JSONObject: ["value": "/a/b"]).value, URL(string: "/a/b"))
+        XCTAssertThrowsError(try UrlModel(JSONObject: ["value": 123]).value)
+    }
+    
+    
+    struct DateModel: Mappable {
+        let value: Date
+        init(map: Mapper) throws {
+            value = try map.from("value")
+        }
+    }
+    
+    func testDate() {
+        XCTAssertEqual(try? DateModel(JSONObject: ["value": "2018-06-03T13:11:50+08:00"]).value, Date(timeIntervalSince1970: 1528002710))
+        XCTAssertThrowsError(try DateModel(JSONObject: ["value": "123123123"]))
+    }
 
 }
